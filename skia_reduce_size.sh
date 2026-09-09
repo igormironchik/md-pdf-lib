318add23d24fba049745616a7d4369a7e71134e7
Igor Mironchik <igor.mironchik@gmail.com>
2026-09-09 13:22:58 +0300

    Reduce Skia sources size

--- skia_reduce_size.sh
+++ skia_reduce_size.sh
@@ -73,3 +73,4 @@ rm -rf 3rdparty/Skia/third_party/externals/vulkanmemoryallocator
 rm -rf 3rdparty/Skia/third_party/externals/vulkan-tools
 rm -rf 3rdparty/Skia/third_party/externals/vulkan-utility-libraries
 rm -rf 3rdparty/Skia/third_party/externals/webgpu-headers
+rm -rf 3rdparty/Skia/tools
